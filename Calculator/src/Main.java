521e63a2ec4184af846760a3f3dd0b941374ebfd
Kirill Gorkiy <gorkiy@list.ru>
2025-05-02 12:49:38 +0200

    [1] Начало создания калькулятора

---
+++ Calculator/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Калькулятор");
+        String value = "1+1";
+        calculateValue(value);
+    }
+
+    private static void calculateValue(String value) {
+        System.out.println("посчитали:" + value);
+    }
+}
