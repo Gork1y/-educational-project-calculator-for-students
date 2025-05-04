d364803589507cb1f995e2cf06bbedb044f0ed4c
BlackFly25 <blackfly25@gmail.com>
2025-05-04 13:09:05 +0200

    [2]Добавлены бизнес Exception

--- Calculator/src/BuisnessException.java
+++ Calculator/src/BuisnessException.java
@@ -1,2 +1,27 @@
+/*
+
+Отдельно созданные Business исключения, для работы нашей программы.
+
+ */
+
 public class BuisnessException {
+}
+
+class RykaLicoException extends Exception {
+    RykaLicoException(String message) {
+        super(message);
+    }
+
+}
+
+class IlligalInputDataException extends Exception {
+    IlligalInputDataException(String message) {
+        super(message);
+    }
+}
+
+class ChtoToNeTakException extends Exception {
+    ChtoToNeTakException(String message) {
+        super(message);
+    }
 }
